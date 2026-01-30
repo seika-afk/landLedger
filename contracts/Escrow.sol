e61224ebefea1e3e052ebeca0df233fca62de6ee
Seika <artistnoob6@gmail.com>
2026-01-30 14:05:18 +0530

    added real_estate smart contract - added connection with escrow

---
+++ contracts/Escrow.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+interface IERC721 {
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _id
+    ) external;
+}
+
+contract Escrow {
+	address public lender;
+	address public inspector;
+	address payable public seller;
+	address public nftAddress;   
+
+	constructor(address _nftAddress, address payable _seller, address _inspector, address _lender){
+		lender = _lender;
+		inspector = _inspector;
+		seller = _seller;
+		nftAddress = _nftAddress;   	}
+
+ }
+
---
+++ test/Escrow.js
@@ -0,0 +1,88 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const tokens = (n) => {
+    return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+describe('Escrow', () => {
+		let buyer ,seller,inspector,lender;
+		let realEstate,escrow
+//all the tests for escrow smart contract
+
+	beforeEach(async ()=>{
+	
+		[buyer,seller,inspector,lender]=await ethers.getSigners() 
+		//console.log(signers)
+		//const  buyer=signers[0]
+		//const seller=signers[1]
+		//deploy realstate
+		const RealEstate = await ethers.getContractFactory('RealEstate');
+		 realEstate = await RealEstate.deploy();
+		await realEstate.deployed();
+		//console.log(realEstate.address);
+			
+		//mint
+		let transaction = await realEstate.connect(seller).mint("https://ipfs.io/ipfs/QmQVcpsjrA6cr1iJjZAodYwmPekYgbnXGo4DFubJiLc2EB/1.json")
+		
+		await transaction.wait()
+
+
+
+		const Escrow=await ethers.getContractFactory('Escrow')
+		escrow= await Escrow.deploy(
+
+realEstate.address,
+			seller.address,
+			inspector.address,
+			lender.address
+		)
+
+
+
+
+	})
+
+	describe('Deployment',()=>{
+
+	it('Returns NFT Address',async ()=>{
+
+
+		 result=await escrow.nftAddress();
+		expect(result).to.be.equal(realEstate.address)
+
+	})
+
+	it('Returns seller',async ()=>{
+	
+
+
+		result=await escrow.seller()
+		expect(result).to.be.equal(seller.address)
+
+
+	})
+		it('Returns lender',async ()=>{
+	
+
+
+
+		result=await escrow.lender()
+		expect(result).to.be.equal(lender.address)
+	})
+
+	it('Returns inspector',async ()=>{
+	
+
+
+
+		result=await escrow.inspector()
+		expect(result).to.be.equal(inspector.address)
+	})
+
+	}) 
+
+
+	//it('saves the addreses',async()=>{});
+
+})
